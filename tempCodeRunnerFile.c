ec108145f11494a005d0dd1e6d0a804bc15f5305
Shashank Shekhar Singh <shashankss011@gmail.com>
2020-09-07 17:32:36 +0530

    Delete tempCodeRunnerFile.c

--- tempCodeRunnerFile.c
+++
@@ -1,6 +0,0 @@
-root = Insert(root,15);
-    // root = Insert(root,20);
-    // root = Insert(root,10);
-    // root = Insert(root,25);
-    // root = Insert(root,18);
-    // root = Insert(root,17);
